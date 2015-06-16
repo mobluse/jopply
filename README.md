860b9f6629e692fd59203c1eb735502a8e5ba5af
Mikael Bonnier <mikael.bonnier@gmail.com>
2015-06-16 20:26:11 +0200

    Updated.

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 # Jopply
 Jopply (Job Apply) helps applying for jobs. This is a very early release
 and everything will change. The rest of the text is in Swedish. The
-command-line arguments are also in Swedish and is the same as in
+command-line arguments are also in Swedish and are the same as in
 the specification, but English arguments will be added in the future.
 
 ##Exempel:
+#### Visar den inbyggda hjälpen.
+###./jopply.pl --help
+
+#### Visar alla län med läns-ID.
+###./jopply.pl --lanid
+
 #### Visar annonsid för jobbannonser i Stockholms län som innehåller CAD och 3D.
-###./jopply.pl -n=CAD,3D -l=1
+###./jopply.pl --n=CAD,3D --l=1
 
 #### Visar annonsid och e-postadress för jobbannonser i Stockholms län som innehåller CAD och som är sökbara via e-post.
-###./jopply.pl -n=CAD -l=1 -e
+###./jopply.pl --nyckelord=CAD --lanid=1 --epostadress
+
+#### Visar annons med annonsid.
+###./jopply.pl --annonsid=2621881
--- jopply.pl
+++ jopply.pl
@@ -8,16 +8,25 @@ use Encode qw(decode encode);
 use WWW::Curl::Easy;
 use JSON;
 use Data::Dumper;
+use Pod::Usage;
+my $man = 0;
+my $help = 0;
 
 my $nyckelord = 'CAD,3D';
 my $lanid = '';
 my $ansokan_epostadress = '';
+my $ansokan_webbadress = '';
 my $annonsid = '';
-GetOptions ('nyckelord=s' => \$nyckelord,
-            'lanid:i' => \$lanid,
-            'epostadress' => \$ansokan_epostadress,
-            'annonsid=s' => \$annonsid)
-  or die("Fel i kommandoradsargument.\n");
+GetOptions('help|?' => \$help, man => \$man,
+           'nyckelord|keyword=s' => \$nyckelord,
+           'lanid:i' => \$lanid,
+           'epostadress' => \$ansokan_epostadress,
+           'webbadress' => \$ansokan_webbadress,
+           'annonsid=s' => \$annonsid)
+  or pod2usage(2);
+pod2usage(1) if $help;
+pod2usage(-exitval => 0, -verbose => 2) if $man;
+
 $nyckelord = join(' ', split(/,/, $nyckelord));
 $nyckelord = uri_escape($nyckelord);
 my $curl = WWW::Curl::Easy->new;
@@ -59,21 +68,41 @@ $decoded_json = decode_json($response_body);
 #  print encode('utf-8', decode('iso-8859-1', $elem->{'namn'})) . "\n";
 #}
 
-my @annonsid = values $decoded_json->{'matchningslista'}{'matchningdata'};
 my $total = 0;
 my $line = 0;
+my @annonsid = values $decoded_json->{'matchningslista'}{'matchningdata'};
 foreach my $elem (@annonsid) {
   #print("$elem->{'annonsid'}\n");
   ++$total;
   $curl->setopt(CURLOPT_URL, "$URL/$elem->{'annonsid'}");
-  if($ansokan_epostadress) {
+  if($ansokan_epostadress || $ansokan_webbadress) {
     $response_body = '';
     $retcode = $curl->perform;
     $decoded_json = decode_json($response_body);
     my $epostadress = $decoded_json->{'platsannons'}{'ansokan'}{'epostadress'};
-    if ($epostadress) {
+    my $webbadress = $decoded_json->{'platsannons'}{'ansokan'}{'webbplats'};
+    my $b_line = 0;
+    if ($ansokan_epostadress && $epostadress) {
+      $b_line = 1;
       ++$line;
-      print("$line: $elem->{'annonsid'}: $epostadress\n");
+      print("$line: $elem->{'annonsid'}: $epostadress");
+    }
+    if ($ansokan_webbadress && $webbadress) {
+      if ($b_line) {
+        print(": $webbadress\n");
+      }
+      else {
+        $b_line = 1;
+        ++$line;
+        print("$line: $elem->{'annonsid'}: : $webbadress\n");
+      }
+    }
+    else {
+      if ($b_line) {
+        print("\n");
+      }
+    }
+    if ($b_line) {
       print(Dumper $elem);
       print(Dumper $decoded_json->{'platsannons'}{'ansokan'});
     }
@@ -87,3 +116,56 @@ foreach my $elem (@annonsid) {
 print("$line/$total=".sprintf('%.2f', 100*$line/$total)."%\n");
 
 __END__
+
+=head1 NAME
+
+sample - Using Getopt::Long and Pod::Usage
+
+=head1 SYNOPSIS
+
+./jopply.pl [options ...]
+
+Options:
+  --help brief help message
+  --man full documentation
+  --lanid=14 läns-ID
+  --nyckelord=CAD,3D lista med söktermer
+  --epost visa jobb som söks via e-post
+  --webb visa jobb som söks via webbtjänst
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-man>
+
+Prints the manual page and exits.
+
+=item B<-lanid>
+
+Väljer läns-ID. Om ID utelämnas så visas en lista med ID och län.
+
+=item B<-nyckelord>
+
+Lista med nyckelord/sökord som separeras med komma.
+
+=item B<-epost>
+
+Visa jobb som kan sökas via e-post och e-postadressen.
+
+=item B<-webb>
+
+Visa jobb som söks via webbtjänst.
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will read the given input file(s) and do something
+useful with the contents thereof.
+
+=cut
