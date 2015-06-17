c16e8bc414c45e0c7b6738eb57d5133207319d94
Mikael Bonnier <mikael.bonnier@gmail.com>
2015-06-17 12:23:49 +0200

    test.bat added

---
+++ test.bat
@@ -0,0 +1,8 @@
+perl jopply.pl
+perl jopply.pl --help
+perl jopply.pl --lanid
+perl jopply.pl --n=CAD,3D --l=12
+perl jopply.pl --nyckelord=CAD,3D --lanid=12 --epost --verbose
+perl jopply.pl --keyword=CAD,3D --lanid=12 --web
+perl jopply.pl -k=CAD,3D -l=12 -w -e
+perl jopply.pl -a=2621881
